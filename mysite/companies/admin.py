530a0527b519ad867ee2f2aa38a56593942e41af
Satyajeet Pal <psatyajeet@gmail.com>
2013-01-14 01:13:01 -0500

    Change index page to show table of companies

--- mysite/companies/admin.py
+++ mysite/companies/admin.py
@@ -2,11 +2,7 @@ from companies.models import Company
 from django.contrib import admin
 
 class CompanyAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['name']}),
-        ('Contact information', {'fields': ['address','phone_number'],'classes': ['collapse']}),
-        ('Android information', {'fields': ['android_users']}),
-    ]
+
     list_display = ('name', 'pub_date', 'address')
     list_filter = ['name']
     search_fields = ['name']
--- mysite/companies/models.py
+++ mysite/companies/models.py
@@ -4,7 +4,21 @@ class Company(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    
+    total_funding=models.IntegerField()
+    founding_date=models.DateField('founding date')
+    benchmark_date=models.DateField('benchmark date')
+    alexa_rank_global=models.IntegerField()
+    employees=models.IntegerField()
     android_users = models.IntegerField()
+    android_user_rating=models.FloatField()
+    twitter_followers=models.IntegerField()
+    twitter_sentiment=models.IntegerField()
+    twitter_tweets=models.IntegerField()
+    facebook_likes=models.IntegerField()
+    facebook_sentiment=models.IntegerField()
+    facebook_messages=models.IntegerField()
+    
+    pub_date = models.DateTimeField('date published')
     def __unicode__(self):
         return self.name
