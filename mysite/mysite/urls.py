bc11795b099f79b789bcf82e4e2cf933c7383663
Satyajeet Pal <psatyajeet@gmail.com>
2013-01-17 10:01:17 -0500

    Change urls.py so that homepage is index.html and default url points to homepage

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'companies.views.index'),
     url(r'^companies/', include('companies.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
