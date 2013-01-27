347caeec07b47a50f5a4a11ba938b77f1b9dafac
Satyajeet Pal <psatyajeet@gmail.com>
2013-01-26 23:27:02 -0500

    Add comments for authentication

--- mysite/companies/views.py
+++ mysite/companies/views.py
@@ -7,9 +7,11 @@ from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 
 def index(request):
+    # If not authenticated, redirect to a nonuser home page.
     if not request.user.is_authenticated():
         return render_to_response('companies/index-nonuser.html')
-    
+        
+    # If authenticated, show list of all companies
     company_list = Company.objects.all().order_by('name')
     return render_to_response('companies/index.html', {'company_list': company_list})
 
@@ -21,12 +23,14 @@ def detail(request, company_id):
     return render_to_response('companies/detail.html', {'company': c})
     
 def login_view(request):
+    # If not authenticated, then redirect to a login page which then redirects back to         originally requested page
     if not request.user.is_authenticated():
         return HttpResponseRedirect('/accounts/login/?next=%s' % request.path)
     
+    # If already authenticated, redirect to home page
     return HttpResponseRedirect('/')    
     
 def logout_view(request):
+    # Logout user and return to homepage
     logout(request)
-    
     return HttpResponseRedirect('/')
