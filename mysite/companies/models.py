41bde637e2d4cfbc2a34d3ad9f05fb66b07287e5
Satyajeet Pal <psatyajeet@gmail.com>
2013-01-31 23:25:09 -0500

    Remove hardcoding to display fields in detail page

--- mysite/companies/models.py
+++ mysite/companies/models.py
@@ -2,23 +2,29 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, blank=True)
+    phone_number = models.CharField(max_length=200, blank=True)
     
-    total_funding=models.IntegerField()
-    founding_date=models.DateField('founding date')
-    benchmark_date=models.DateField('benchmark date')
-    alexa_rank_global=models.IntegerField()
-    employees=models.IntegerField()
-    android_users = models.IntegerField()
-    android_user_rating=models.FloatField()
-    twitter_followers=models.IntegerField()
-    twitter_sentiment=models.IntegerField()
-    twitter_tweets=models.IntegerField()
-    facebook_likes=models.IntegerField()
-    facebook_sentiment=models.IntegerField()
-    facebook_messages=models.IntegerField()
+    total_funding=models.IntegerField('Total Funding')
+    founding_date=models.DateField('Founding Date')
+    benchmark_date=models.DateField('Benchmark Date', blank=True)
+    alexa_rank_global=models.IntegerField('Alexa Rank', blank=True)
+    employees=models.IntegerField('Num. Employees', blank=True)
+    android_users = models.IntegerField('Num. Android Users', blank=True)
+    android_user_rating=models.FloatField('Android User Rating', blank=True)
+    twitter_followers=models.IntegerField('Num. Twitter Followers', blank=True)
+    twitter_sentiment=models.IntegerField('Twitter Positive Sentiment', blank=True)
+    twitter_tweets=models.IntegerField('Num. Tweets', blank=True)
+    facebook_likes=models.IntegerField('Num. Facebook Likes', blank=True)
+    facebook_sentiment=models.IntegerField('Facebook Positive Sentiment', blank=True)
+    facebook_messages=models.IntegerField('Num. Facebook Messages', blank=True)
     
+    def get_fields(self):
+        # make a list of field/values.
+        return [(field.verbose_name, field.value_to_string(self)) for field in self._meta.fields]
+        
     #pub_date = models.DateTimeField('date published')
     def __unicode__(self):
         return self.name
+        
+
